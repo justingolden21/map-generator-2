223451661ce7ed1e9521c469c2fd8f419c778594
RGB Studios <justingolden21@gmail.com>
2021-04-26 22:42:02 -0700

    add color settings

--- js/main.js
+++ js/main.js
@@ -22,9 +22,19 @@ function generate() {
 	let percent = validate('percent',50);
 	let smoothing = validate('smoothing',10);
 	let size = validate('size',5);
+	const colorSettings = {
+		whue: validate('whue',210),
+		wbase: validate('wbase',80),
+		wsat: validate('wsat',60),
+		wmult: validate('wmult',4),
+		thue: validate('thue',140),
+		tbase: validate('tbase',80),
+		tsat: validate('tsat',20),
+		tmult: validate('tmult',2)
+	};
 
 	let grid = createCellularMap(width, height, percent, smoothing);
-	drawGrid(grid, size);
+	drawGrid(grid, size, colorSettings);
 }
 
 function validate(name, defaultVal) {
@@ -38,25 +48,20 @@ function check(num, min, max, defaultVal) {
 	return isNaN(num) ? defaultVal : num;
 }
 
-function drawGrid(grid, size) {
+function drawGrid(grid, size, colorSettings) {
 	const width = grid.length, height = grid[0].length;
 	canvas.width = width*size;
 	canvas.height = height*size;
 	for(let x=0; x<width; x++) {
 		for(let y=0; y<height; y++) {
-			ctx.fillStyle = getColor(grid, x, y);
+			ctx.fillStyle = getColor(grid, x, y, colorSettings);
 			ctx.fillRect(x*size, y*size, size, size);
 		}
 	}
 }
 
-function getColor(grid, x, y) {
-	// https://justingolden.me/tailwind-colors/
-	// const colors = '#0C4A6E #075985 #0369A1 #0EA5E9 #7DD3FC #FDE68A #86EFAC #22C55E #15803D'.split(' ').reverse();
-	// let idx = surroundingCount(grid, x, y) >> 1; // divide by 2
-	// idx += (grid[x][y] == 0 ? 0 : 4);
-	// return colors[idx];
-
+function getColor(grid, x, y, s) {
 	let count = surroundingCount(grid, x, y);
-	return grid[x][y] == 1 ? `hsl(210, 80%, ${60-count*4}%)` : `hsl(140, 80%, ${20+count*2}%)`;
+	// return grid[x][y] == 1 ? `hsl(210, 80%, ${60-count*4}%)` : `hsl(140, 80%, ${20+count*2}%)`;
+	return grid[x][y] == 1 ? `hsl(${s.whue}, ${s.wsat}%, ${s.wbase-count*s.wmult}%)` : `hsl(${s.thue}, ${s.tsat}%, ${s.tbase+count*s.tmult}%)`;
 }

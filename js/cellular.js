e626a5eff5f4fce007456d265220fbfd52426adc
RGB Studios <justingolden21@gmail.com>
2021-04-26 20:52:40 -0700

    extrapolate functions in cellular.js

--- js/cellular.js
+++ js/cellular.js
@@ -10,39 +10,41 @@ function createCellularMap(width, height, percent, smoothing) {
 
 	// smooth grid
 	for(let i=0; i<smoothing; i++) {
-		smooth();
-	}
-
-	function randBool(oddsOfTrue) {
-		return Math.random() >= oddsOfTrue/100 ? 0 : 1;
-	}
-
-	function smooth() {
-		for(let x=0; x<width; x++) {
-			for(let y=0; y<height; y++) {
-				let count = surroundingCount(x, y);
-				// set current point to average of surrounding
-				grid[x][y] = count > 4 ? 1 : count < 4 ? 0 : grid[x][y];
-			}
-		}
-	}
-
-	function surroundingCount(xPos, yPos) {
-		let count = 0;
-		for(let x = xPos-1; x <= xPos+1; x++) {
-			for(let y = yPos-1; y <= yPos+1; y++) {
-				if(x >= 0 && x < width && y >= 0 && y < height) { // inside
-					if(!(x == xPos && y == yPos) ) { //don't count current tile
-						count += grid[x][y]; // add 1 if true, else 0
-					}
-				} else { // outside, more walls on sides
-					count++;
-				}
-			}
-		}
-		return count;
+		grid = smooth(grid);
 	}
 
 	return grid;
+}
 
+function randBool(oddsOfTrue) {
+	return Math.random() >= oddsOfTrue/100 ? 0 : 1;
+}
+
+function smooth(grid) {
+	const width = grid.length, height = grid[0].length;
+	for(let x=0; x<width; x++) {
+		for(let y=0; y<height; y++) {
+			let count = surroundingCount(grid, x, y);
+			// set current point to average of surrounding
+			grid[x][y] = count > 4 ? 1 : count < 4 ? 0 : grid[x][y];
+		}
+	}
+	return grid;
+}
+
+function surroundingCount(grid, xPos, yPos) {
+	const width = grid.length, height = grid[0].length;
+	let count = 0;
+	for(let x = xPos-1; x <= xPos+1; x++) {
+		for(let y = yPos-1; y <= yPos+1; y++) {
+			if(x >= 0 && x < width && y >= 0 && y < height) { // inside
+				if(!(x == xPos && y == yPos) ) { //don't count current tile
+					count += grid[x][y]; // add 1 if true, else 0
+				}
+			} else { // outside, more walls on sides
+				count++;
+			}
+		}
+	}
+	return count;
 }
